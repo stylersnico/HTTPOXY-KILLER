06605efd1f7624da7828f43318ca8b9f753f6b83
Nicolas Simond <nicolassimond@posteo.ch>
2016-07-21 10:57:23 +0200

    Update httpoxy-fix.debian.sh

--- httpoxy-fix.debian.sh
+++ httpoxy-fix.debian.sh
@@ -5,8 +5,7 @@ if [[ "$EUID" -ne 0 ]]; then
   	exit 1
 fi
 
-echo "This script is made to work under Debian systems \
-Let's check your release now."
+echo "This script is made to work under Debian systems, Let's check your release now."
 
 if [ -f /etc/debian_version ]; then
     echo "Good Debian system detected, now, see if you use NGINX or Apache :
@@ -19,7 +18,7 @@ fi
 if [ -f /etc/apache2/apache2.conf ]; then
     echo "Apache2 Found, fixing the httpoxy vulnerability .... \"
     a2enmod headers
-    wget https://raw.githubusercontent.com/stylersnico/HTTPOXY-KILLER/master/etc/apache2/conf-available/httpoxy.conf
+    wget -q https://raw.githubusercontent.com/stylersnico/HTTPOXY-KILLER/master/etc/apache2/conf-available/httpoxy.conf
     mv httpoxy.conf /etc/apache2/conf-available/httpoxy.conf
     a2enconf httpoxy
     service apache2 restart
@@ -30,7 +29,8 @@ fi
 
 if [ -f /etc/nginx/nginx.conf ]; then
     echo "NGINX Found, fixing the httpoxy vulnerability ... \"
-    echo 'fastcgi_param HTTP_PROXY "";' >> /etc/nginx/fastcgi_params
+    wget -q https://raw.githubusercontent.com/stylersnico/HTTPOXY-KILLER/master/etc/nginx/fastcgi_params
+    cat fastcgi_params >> /etc/nginx/fastcgi_params
     service nginx restart
     echo "Alright, the httpoxy vulnerability is fixed."
 else
