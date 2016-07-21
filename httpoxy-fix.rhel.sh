926de24b250af5f178eddfa93a05560392c4e374
Nicolas Simond <nicolassimond@posteo.ch>
2016-07-21 10:46:07 +0200

    Create httpoxy-fix.rhel.sh

---
+++ httpoxy-fix.rhel.sh
@@ -0,0 +1,36 @@
+# Check root access
+if [[ "$EUID" -ne 0 ]]; then
+	echo -e "${CRED}Sorry, you need to run this as root${CEND}"
+  	exit 1
+fi
+
+echo "This script is made to work under Debian systems \
+Let's check your release now."
+
+if [ -f /etc/redhat-release ]; then
+    echo "Good RHEL system detected, now, see if you use NGINX or Apache :
+	
+else
+    echo "No compatible OS detected, exiting ..."
+    exit 0
+fi
+
+if [ -f /etc/httpd/conf/httpd.conf ]; then
+    echo "Apache2 Found, fixing the httpoxy vulnerability .... \"
+    echo "RequestHeader unset Proxy early" >> /etc/httpd/conf/httpd.conf
+    service httpd restart
+    echo "Alright, the httpoxy vulnerability is fixed."
+
+else
+    echo "No Apache2 detected or not using standard folders"
+fi
+
+if [ -f /etc/nginx/nginx.conf ]; then
+    echo "NGINX Found, fixing the httpoxy vulnerability ... \"
+    echo 'fastcgi_param HTTP_PROXY "";' >> /etc/nginx/fastcgi_params
+    service nginx restart
+    echo "Alright, the httpoxy vulnerability is fixed."
+
+else
+    echo "No NGINX detected or not using standard folders"
+fi
